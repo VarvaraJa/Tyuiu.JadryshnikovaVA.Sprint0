1a70fe8db862f6ae6a84c2132e1ced47faf72aff
Jadryshnikova <Tyuiu.Jadryshnikova@outlook.com>
2023-10-01 23:12:43 +0500

    Task2

---
+++ Tyuiu.JadryshnikovaVA.Sprint0.Task2.V0.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.JadryshnikovaVA.Sprint0.Task2.V0.Lib
+{
+    public class DataService
+    {
+        public static string GetMesssage(string name)
+        {
+            return $"Привет, {name}";
+        }
+    }
+}
---
+++ Tyuiu.JadryshnikovaVA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.JadryshnikovaVA.Sprint0.Task2.V0;
+
+namespace Tyuiu.JadryshnikovaVA.Sprint0.Task2.V0.Lib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Варвара"));
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+}
